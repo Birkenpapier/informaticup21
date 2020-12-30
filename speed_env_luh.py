0e37ddb9b51c46ec8e9d7781e628fade787df5ae
Kevin Peivareh <kevin@peivareh.com>
2020-12-30 15:45:24 +0100

    cleaned up get_state function

--- speed_env_luh.py
+++ speed_env_luh.py
@@ -155,22 +155,8 @@ class Speed():
 
     def get_state_speed(self):
         # snake coordinates abs
-        # self.snake.x, self.snake.y = self.snake.xcor()/WIDTH, self.snake.ycor()/HEIGHT   
-        # spe_ed
         player_x, player_y = self.player.x, self.player.y
 
-        # snake coordinates scaled 0-1
-        # self.snake.xsc, self.snake.ysc = self.snake.x/WIDTH+0.5, self.snake.y/HEIGHT+0.5
-        # spe_ed
-        player_xsc, player_ysc = self.player.x / self.gamestate.width + 0.5, self.player.y / self.gamestate.height + 0.5 # TODO: implement usage of var
-
-
-        # apple coordintes scaled 0-1 
-        # self.apple.xsc, self.apple.ysc = self.apple.x/WIDTH+0.5, self.apple.y/HEIGHT+0.5
-        # spe_ed
-        # not scaled
-        # enemy_x, enemy_y = self.gamestate.players[0].x, self.gamestate.players[0].y
-        
         # take closest enemy instead of the first enemy
         previous_distance = 2000
         closest_enemy = 0
@@ -187,13 +173,6 @@ class Speed():
 
         enemy_x, enemy_y = self.gamestate.players[closest_enemy - 1].x, self.gamestate.players[closest_enemy - 1].y
 
-        """
-        if self.gamestate.players[0].id != self.player.id:
-            enemy_x, enemy_y = self.gamestate.players[0].x, self.gamestate.players[0].y
-        else:
-            enemy_x, enemy_y = self.gamestate.players[1].x, self.gamestate.players[1].y
-        """
-
         # wall check
         if player_y >= self.gamestate.height / 2:
             wall_up, wall_down = 1, 0
@@ -209,54 +188,8 @@ class Speed():
             wall_right, wall_left = 0, 0
 
         # check how we can implement the bodylength, we definetly need it -> Dominik implemented this
-        """
-        # body close
-        body_up = []
-        body_right = []
-        body_down = []
-        body_left = []
-        if len(self.snake_body) > 3:
-            for body in self.snake_body[3:]:
-                if body.distance(self.snake) == 20:
-                    if body.ycor() < self.snake.ycor():
-                        body_down.append(1)
-                    elif body.ycor() > self.snake.ycor():
-                        body_up.append(1)
-                    if body.xcor() < self.snake.xcor():
-                        body_left.append(1)
-                    elif body.xcor() > self.snake.xcor():
-                        body_right.append(1)
-        
-        if len(body_up) > 0: body_up = 1
-        else: body_up = 0
-        if len(body_right) > 0: body_right = 1
-        else: body_right = 0
-        if len(body_down) > 0: body_down = 1
-        else: body_down = 0
-        if len(body_left) > 0: body_left = 1
-        else: body_left = 0
-        """
-
+        # TODO: check whether it's good to determine between wall and enemy made obstacle
         # spe_ed_state: enemy_up, enemy_right, enemy_down, enemy_left, obstacle_up, obstacle_right, obstacle_down, obstacle_left, direction_up, direction_right, direction_down, direction_left (are we needing an action here?)
-        """
-        if self.env_info['state_space'] == 'coordinates':
-            state = [self.apple.xsc, self.apple.ysc, self.snake.xsc, self.snake.ysc, \
-                    int(wall_up or body_up), int(wall_right or body_right), int(wall_down or body_down), int(wall_left or body_left), \
-                    int(self.snake.direction == 'up'), int(self.snake.direction == 'right'), int(self.snake.direction == 'down'), int(self.snake.direction == 'left')]
-        elif self.env_info['state_space'] == 'no direction':
-            state = [int(self.snake.y < self.apple.y), int(self.snake.x < self.apple.x), int(self.snake.y > self.apple.y), int(self.snake.x > self.apple.x), \
-                    int(wall_up or body_up), int(wall_right or body_right), int(wall_down or body_down), int(wall_left or body_left), \
-                    0, 0, 0, 0]
-        elif self.env_info['state_space'] == 'no body knowledge':
-            state = [int(self.snake.y < self.apple.y), int(self.snake.x < self.apple.x), int(self.snake.y > self.apple.y), int(self.snake.x > self.apple.x), \
-                    wall_up, wall_right, wall_down, wall_left, \
-                    int(self.snake.direction == 'up'), int(self.snake.direction == 'right'), int(self.snake.direction == 'down'), int(self.snake.direction == 'left')]
-        else:
-            state = [int(self.snake.y < self.apple.y), int(self.snake.x < self.apple.x), int(self.snake.y > self.apple.y), int(self.snake.x > self.apple.x), \
-                    int(wall_up or body_up), int(wall_right or body_right), int(wall_down or body_down), int(wall_left or body_left), \
-                    int(self.snake.direction == 'up'), int(self.snake.direction == 'right'), int(self.snake.direction == 'down'), int(self.snake.direction == 'left')]
-        """ 
-
         state = [int(player_y < enemy_y), int(player_x < enemy_x), int(player_y > enemy_y), int(player_x > enemy_x), \
                 int(wall_up), int(wall_right), int(wall_down), int(wall_left), \
                 int(self.player.direction == 'up'), int(self.player.direction == 'right'), int(self.player.direction == 'down'), int(self.player.direction == 'left')]
